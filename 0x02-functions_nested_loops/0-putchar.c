07e954dc8ef3fdaa582b49bcba74da53cd3ed019
zerhouninfo <zerhouninfo1@gmail.com>
2023-03-22 17:07:27 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+# inclure  " main.h "
+/* *
+* principal - imprimer _putchar
+* est écrit par mbah
+* Retour : 0
+ */
+
+int  principal ( vide )
+{
+_putchar ( ' _ ' );
+_putchar ( ' p ' );
+_putchar ( ' u ' );
+_putchar ( ' t ' );
+_putchar ( ' c ' );
+_putchar ( ' h ' );
+_putchar ( ' un ' );
+_putchar ( ' r ' );
+_putchar ( ' \n ' );
+retour ( 0 );
+}
